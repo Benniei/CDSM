5544fc299179a75973e04a32646ad9bfa88a586f
Richard Chan <85211612+RC-Archive@users.noreply.github.com>
2022-10-12 04:08:11 -0400

    Added Passport integration for Google OAuth2.0 - Passport serves as middleware for authentication and creating Users - authRouter.js handles routing for [login] authentication with Google

---
+++ server/routes/authRouter.js
@@ -0,0 +1,20 @@
+// Import modules
+const express = require('express');
+const passport = require('passport');
+
+// Create router instance
+const router = express.Router();
+
+// Scope options to retrieve the user's Google profile and email addresses
+const scope = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'];
+
+// Attempt authentication with Google during login; "prompt: 'select_account'" forces account selection screen to appear every time
+router.get('/auth/google', passport.authenticate('google', { prompt: 'select_account', scope: scope } ));
+// Redirect based on Google authentication response
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: process.env.CLIENT_BASE_URL }), function(req, res) {
+    // Upon successful authentication, redirect to dashboard
+    res.redirect(process.env.CLIENT_BASE_URL + '/dashboard');
+    console.log("Successfully authenticated with google.");
+});
+
+module.exports = router;
---
+++ server/services/passport.js
@@ -0,0 +1,46 @@
+// Import modules
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/user-model');
+
+// Serialize and deserialize user information to support sessions
+passport.serializeUser(function(user, done) {
+    done(null, user.id)
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+// Passport for Google OAuth2.0
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_AUTH_REDIRECT_URL
+},
+async function(accessToken, refreshToken, profile, done) {
+    try{
+        // Check if the User already exists
+        const existingUser = await User.findOne({ profileId: profile.id });
+        // If they do, attempt to redirect them to the dashboard immediately
+        if (existingUser) {
+            done(null, existingUser);
+        } else {
+            // If not, create a new User and add it to the profile database before attempting to redirect the user
+            const newUser = new User({
+                profileId: profile.id,
+                cloudProvider: profile.provider,
+                displayName: profile.displayName,
+                email: profile.emails[0].value,
+                threshold: 0.8
+            });
+            const savedUser = await newUser.save();
+            console.log(`Added User ${savedUser.displayName} to database`);
+            done(null, newUser);
+        }
+    }  catch (error) {
+        console.error(error);
+    }
+}));
