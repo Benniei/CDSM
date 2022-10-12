837bfe3a3c759e934e49f3b712cc57bbaaecbf07
Bennie Chen <Bennie.chen09@gmail.com>
2022-10-12 17:57:13 -0400

    Landing Page with Login to Drive and OutLook

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,28 +1,27 @@
 // Local imports
 import { ReactComponent as GoogleDrive } from '../../assets/GoogleDriveIcon.svg';
+import { ReactComponent as MicrosoftOneDriveIcon } from '../../assets/MicrosoftOneDriveIcon.svg';
 
 // Imports from Material-UI
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import SvgIcon from '@mui/material/SvgIcon';
+import {Box, Typography} from '@mui/material'
 
 function LandingPage() {
-    // Custom SvgIcon for the Google Drive logo
-    const GoogleDriveIcon = (
-        <SvgIcon>
-            <GoogleDrive/>
-        </SvgIcon>
-    );
 
     return (
-        <Container>
-            <Box>
-                {/* Login button for Google - Redirects to Dashboard screen */}
-                <Button href = 'http://localhost:4000/auth/google' variant = 'contained' startIcon = {GoogleDriveIcon}>Google Drive</Button>
-                LandingPage
-            </Box>   
-        </Container>
+        <div id="splash-screen">
+            <b>Kloud</b>
+            <br></br>
+            {/* Login Button for Google Drive - Redirects to Dashboard Screen */}
+            <Box button className="splash-button-outer" onClick={event => window.location.href='http://localhost:4000/auth/google'} mt={3}>
+                <GoogleDrive />
+                <Typography inline variant='bold' ml={.5}> <strong> Login with Google Drive </strong> </Typography>
+            </Box>
+            {/* Login Button for Google Drive - Redirects to Dashboard Screen */}
+            <Box button className="splash-button-outer" onClick={event => window.location.href=''}>
+                <MicrosoftOneDriveIcon />
+                <Typography inline variant='bold' ml={.5}> <strong> Login with Microsoft One Drive </strong> </Typography>
+            </Box>
+        </div>
     );
 }
 
