7439927e500ee5fc857c098d93bd0518f43a0e58
Richard Chan <85211612+RC-Archive@users.noreply.github.com>
2022-10-12 04:03:15 -0400

    Renamed server.js to index.js

---
+++ server/index.js
@@ -0,0 +1,37 @@
+// Import modules
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+
+// Load config
+dotenv.config({ path: './config/config.env' });
+const PORT = process.env.PORT || 4000;
+
+// Initialize Express
+const app = express();
+
+// Middleware
+app.use(cookieParser());
+app.use(cors({ origin:true, credentials:true }));                                                   // Enable Cross Origin Resource Sharing
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: true,  }));     // Don't save session if unmodified, don't create session until something is stored
+
+// Initialize Passport
+require('./services/passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Initialize database
+const connectDB = require('./db/mongoose');
+connectDB();
+
+// Routers
+app.use('/', require('./routes/authRouter'));       // Route for cloud service authorization
+app.use("/", require('./routes/router'));
+
+// Set server to listening mode
+app.listen(PORT,"localhost", () => console.log(`Server running on port ${PORT}`));
