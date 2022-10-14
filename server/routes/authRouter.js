52b76062da2acef260698d08156a602bdd20b102
Bennie Chen <Bennie.chen09@gmail.com>
2022-10-14 00:25:59 -0400

    Differentiate between Drive and Microsoft

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -6,10 +6,11 @@ const passport = require('passport');
 const router = express.Router();
 
 // Scope options to retrieve the user's Google profile and email addresses
-const scope = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'];
+const scopeGoogle = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'];
+const scopeMicrosoft = ["user.read"]
 
 // Attempt authentication with Google during login; "prompt: 'select_account'" forces account selection screen to appear every time
-router.get('/auth/google', passport.authenticate('google', { prompt: 'select_account', scope: scope } ));
+router.get('/auth/google', passport.authenticate('google', { prompt: 'select_account', scope: scopeGoogle } ));
 // Redirect based on Google authentication response
 router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: process.env.CLIENT_BASE_URL }), function(req, res) {
     // Upon successful authentication, redirect to dashboard
@@ -17,7 +18,7 @@ router.get('/auth/google/callback', passport.authenticate('google', { failureRed
     console.log("Successfully authenticated with google.");
 });
 
-router.get('/auth/onedrive', passport.authenticate('onedrive'));
+router.get('/auth/onedrive', passport.authenticate('onedrive', {scope: scopeMicrosoft}));
 router.get('/auth/onedrive/callback', passport.authenticate('onedrive', { failureRedirect: process.env.CLIENT_BASE_URL }), function(req, res) {
     // Successful authentication, redirect to dashboard.
     res.redirect(process.env.CLIENT_BASE_URL + '/dashboard');
