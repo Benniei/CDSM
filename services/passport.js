854c513d92d8a92354ddde10b97076c80823f5af
Bennie Chen <Bennie.chen09@gmail.com>
2022-10-15 04:23:54 -0400

    Change Passport Callback URL

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_AUTH_REDIRECT_URL
+    callbackURL: `${process.env.CLIENT_BASE_URL} + /auth/google/callback`
 },
 async function(accessToken, refreshToken, profile, done) {
     try{
@@ -50,7 +50,7 @@ async function(accessToken, refreshToken, profile, done) {
 passport.use(new MicrosoftStrategy({
     clientID: process.env.ONEDRIVE_CLIENT_ID,
     clientSecret: process.env.ONEDRIVE_CLIENT_SECRET,
-    callbackURL: process.env.ONEDRIVE_AUTH_REDIRECT_URL,
+    callbackURL: `${process.env.CLIENT_BASE_URL} + /auth/microsoft/callback`,
     scope: ['user.read']
 },
 async function(accessToken, refreshToken, profile, done) {
