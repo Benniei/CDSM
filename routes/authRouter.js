6be390dc2a428c0bb98cbb1e4f226738130b20f8
Richard Chan <85211612+RC-Archive@users.noreply.github.com>
2022-10-16 06:47:32 -0400

    Added route to properly logout the user - '/logout' route terminates login session and redirects to landing page - Moved scope for Google authentication into method parameter - Added scope for Microsoft authentication into method paramter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,23 +5,43 @@ const passport = require('passport');
 // Create router instance
 const router = express.Router();
 
-// Scope options to retrieve the user's Google profile and email addresses
-const scope = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'];
-
 // Attempt authentication with Google during login; "prompt: 'select_account'" forces account selection screen to appear every time
-router.get('/auth/google', passport.authenticate('google', { prompt: 'select_account', scope: scope } ));
+router.get('/auth/google', passport.authenticate('google', { accessType: 'offline', prompt: 'select_account', scope: ['profile', 'email', 'https://www.googleapis.com/auth/drive'] } ));
 // Redirect based on Google authentication response
 router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: process.env.CLIENT_BASE_URL }), function(req, res) {
     // Upon successful authentication, redirect to dashboard
-    res.redirect(process.env.CLIENT_BASE_URL + 'dashboard');
     console.log("Successfully authenticated with google.");
+    res.redirect(process.env.CLIENT_BASE_URL + 'dashboard');
+    // res.redirect('http://localhost:3000/dashboard');
 });
 
-router.get('/auth/microsoft', passport.authenticate('microsoft', { prompt: 'select_account'} ));
+router.get('/auth/microsoft', passport.authenticate('microsoft', { accessType: 'offline', prompt: 'select_account', scope: 'user.read' } ));
 router.get('/auth/microsoft/callback', passport.authenticate('microsoft', { failureRedirect: process.env.CLIENT_BASE_URL }), function(req, res) {
-    // Successful authentication, redirect to dashboard.
-    res.redirect(process.env.CLIENT_BASE_URL + 'dashboard');
     console.log("Successfully authenticated with microsoft.");
+    res.redirect(process.env.CLIENT_BASE_URL + 'dashboard');
+    // res.redirect('http://localhost:3000/dashboard');
 });
+
+// Terminate login session and redirect the user to the landing page
+router.get('/logout', function(req, res, next) {
+    // Terminate login session
+    req.logout(function(error) {
+        if (error) {
+            return next(error);
+        }
+        // Redirect user to landing page
+        console.log('User logged out.');
+        res.redirect(process.env.CLIENT_BASE_URL);
+        // res.redirect('http://localhost:3000');    
+    });
+});
+
+router.get('/test', function(req, res, next) {
+    if (req.user) {
+        console.log(req.user);
+    } else {
+        console.log('User not authenticated.');
+    }
+})
 
 module.exports = router;
