cfbc9330a79c406f50a5e2834243b214ed59686a
Richard Chan <85211612+RC-Archive@users.noreply.github.com>
2022-10-16 06:44:02 -0400

    Added 'refreshToken' field to User schema

--- models/user-model.js
+++ models/user-model.js
@@ -8,16 +8,17 @@ const GroupSnapshot = require('./groupsnapshot-model');
 // Schema for User
 const UserSchema = new Schema(
     {
-        profileId: { type: String, required: true },            // Unique profile ID taken from cloud provider
-        cloudProvider: { type: String, required: true },        // Drive service selected by the user
-        displayName: { type: String, required: true },          // First and last name taken from cloud provider
-        email: { type: String, required: true },                // Email taken from cloud provider
-        threshold: { type: Number, required: true },            // Threshold value for deviant check
+        profileId: { type: String, required: true },                                            // Unique profile ID taken from cloud provider
+        cloudProvider: { type: String, required: true },                                        // Drive service selected by the user
+        displayName: { type: String, required: true },                                          // First and last name taken from cloud provider
+        email: { type: String, required: true },                                                // Email taken from cloud provider
+        threshold: { type: Number, required: true },                                            // Threshold value for deviant check
+        refreshToken: { type: String, required: false },                                        // Refresh token provided by cloud provider
         filesnapshot: { type: Schema.Types.ObjectId, ref:'FileSnapshot', required: false },
         groupsnapshot: { type: Schema.Types.ObjectId, ref:'GroupSnapshot', required: false },
-        access_control_req: { type: String, required: false}     // access control requirements
+        access_control_req: { type: String, required: false}                                    // access control requirements
     },
-    { timeStamps: true }                                        // Timestamps for when document was created and last updated
+    { timeStamps: true }                                                                        // Timestamps for when document was created and last updated
 );
 
 module.exports = mongoose.model('User', UserSchema);
