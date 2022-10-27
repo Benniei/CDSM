94e9bcbf323b080cb31e97003447da6265230bc2
Richard Chan <85211612+RC-Archive@users.noreply.github.com>
2022-10-27 13:36:49 -0400

    Fixed typo in 'sharedDrives' type

--- models/filesnapshot-model.js
+++ models/filesnapshot-model.js
@@ -9,7 +9,7 @@ const FileSnapshotSchema = new Schema(
         snapshotId: { type: String, required: true },           // Unique Id of the screenshot
         myDrive: { type: [String], required: false },           // Array containing files in the user's primary drive
         sharedWithMe: { type: [String], required: false },      // Array containing files shared with the user
-        sharedDrives: { type: [[Strings]], required: false},    // Array containing files in the user's shared drives
+        sharedDrives: { type: [[String]], required: false},    // Array containing files in the user's shared drives
         metadata: {type: [String], required: false }            // Any additional metadata
     },
     { timeStamps: true }                                        // Timestamps for when screenshot was created and last updated
