b20dba04758d5e4f5e3a2af74d4149ca29a5a060
Richard Chan <85211612+RC-Archive@users.noreply.github.com>
2022-11-20 02:36:39 -0500

    Updated Axios Requests - Added request for getFiles() - Modified function indentation

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,86 +1,74 @@
-import axios from 'axios'
+// Import modules
+import axios from 'axios';
 
-axios.defaults.withCredentials=true;
+// Configure Axios
+axios.defaults.withCredentials = true;
 const api = axios.create({
     baseURL: process.env.REACT_APP_URL
-})
+});
 
-export const test = () => api.get('/test')
-.then(response => {
-    return response
- })
- .catch(error => {
-    return error.response;
- });
-
- /* Authentication */
- export const getLoggedIn = () => api.get(`/loggedIn`)
-.then(response => {
-    return response
- })
- .catch(error => {
-    return error.response
- });
-
- /* Snapshot */
- export const getSnapshot = (payload) => api.post('/snapshot', payload)
- .then(response => {
-    return response
- })
- .catch(error => {
-    return error.response;
- });
-
-export const getFolder = (payload, id, folderid) => api.post('/snapshot/' + id + '/' + folderid, payload)
-.then(response => {
-   return response
-})
-.catch(error => {
+// Authentication related requests
+export const getLoggedIn = () => api.get('/loggedIn').then(response => {
+   return response;
+}).catch(error => {
    return error.response;
 });
 
- export const takeSnapshot = (payload) => api.post('/snapshot/create', payload)
- .then(response => {
-    return response
- })
- .catch(error => {
-    return error.response;
- });
+// Snapshot related requests
+export const getSnapshot = (payload) => api.post('/snapshot', payload).then(response => {
+   return response;
+}).catch(error => {
+   return error.response;
+});
 
- export const buildQuery = (payload) => api.post('/buildQuery', payload)
- .then(response => {
-    return response
- })
- .catch(error => {
-    return error.response;
- });
+export const takeSnapshot = (payload) => api.post('/snapshot/create', payload).then(response => {
+   return response;
+}).catch(error => {
+   return error.response;
+});
 
- export const doQuery = (payload) => api.post('/doQuery', payload)
- .then(response => {
-    return response
- })
- .catch(error => {
-    return error.response;
- });
+// File related requests
+export const getFiles = (payload, snapshotId) => api.post(`/snapshot/${snapshotId}/files`, payload).then(response => {
+   return response;
+}).catch(error => {
+   return error.response;
+});
 
- /* User */
- export const updateACR = (payload) => api.post('/acr', payload)
- .then(response => {
-    return response
- })
- .catch(error => {
-    return error.response;
- });
+export const getFolder = (payload, snapshotId, folderId) => api.post(`/snapshot/${snapshotId}/${folderId}`, payload).then(response => {
+   return response;
+}).catch(error => {
+   return error.response;
+});
+
+// Query related requests
+export const buildQuery = (payload) => api.post('/buildQuery', payload).then(response => {
+   return response;
+}).catch(error => {
+   return error.response;
+});
+
+export const doQuery = (payload) => api.post('/doQuery', payload).then(response => {
+   return response;
+}).catch(error => {
+   return error.response;
+});
+
+// User related requests
+export const updateACR = (payload) => api.post('/acr', payload).then(response => {
+   return response;
+}).catch(error => {
+   return error.response;
+});
  
 const apis = {
-test,
-getLoggedIn,
-getSnapshot,
-getFolder,
-takeSnapshot,
-buildQuery,
-doQuery,
-updateACR
+   buildQuery,
+   doQuery,
+   getFiles,
+   getFolder,
+   getLoggedIn,
+   getSnapshot,
+   takeSnapshot,
+   updateACR
 };
 
 export default apis;
