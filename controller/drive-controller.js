e68e862f0c0e6d7dd33d29445b2113ad42806172
Richard Chan <85211612+RC-Archive@users.noreply.github.com>
2022-11-29 02:57:25 -0500

    Updated Drive and Google Drive Controllers - Changed functional expressions to function declarations - Reworked logic for retrieving file data - Normalized file property names for cloud service provider independence - Moved bulk of createFileSnapshot to Drive Controller for extensibility

--- controller/drive-controller.js
+++ controller/drive-controller.js
@@ -1,48 +1,85 @@
-// Being changed to a wrapper.
-const GoogleDriveController = require('../controller/google-drive-controller');
+// Import modules
+const moment = require('moment');
+
+// Local imports
+const AnalysisController = require('../controller/analyze-controller');
 const File = require('../models/file-model');
 const FileSnapshot = require('../models/filesnapshot-model');
+const GoogleDriveController = require('../controller/google-drive-controller');
 const User = require('../models/user-model');
 
+
 // Creates a FileSnapshot of the user's current drive
-createFileSnapshot = async function(req, res) {
+async function createFileSnapshot(req, res) {
     try {
+        // Retrieve the user's profile
         const user = await User.findById(req.userId, { cloudProvider: 1, filesnapshot: 1, refreshToken: 1, threshold: 1 });
         if (!user) {
             throw new Error('Could not find User in database.');
         }
-        req.user = user;
-        switch (req.user.cloudProvider) {
+        // List of all of the user's drives
+        let driveIds;
+        // Map of all files in the user's drives
+        let fileMap;
+        switch (user.cloudProvider) {
             case 'google':
-                GoogleDriveController.createFileSnapshot(req, res);
+                // Initialize instance of the Google Drive API service
+                const driveAPI = await GoogleDriveController.GD_initializeAPI(user.refreshToken);
+                driveIds = await GoogleDriveController.GD_getDrives(driveAPI);
+                fileMap = await GoogleDriveController.GD_getFileMap(driveAPI, driveIds);
                 break;
             default:
-                throw new Error('Could not find listed cloud provider');
+                throw new Error('Could not find listed cloud provider.');
         }
+        // Create a File Snapshot in our database to obtain a snapshotId
+        const newSnapshot = new FileSnapshot({
+            owner: user._id,
+            snapshotId: user._id + '-' + moment().format('MMMM Do YYYY, H:mm:ss'),
+            driveIds: driveIds
+        });
+        await FileSnapshot.create(newSnapshot);
+        console.log(`Added FileSnapshot '${newSnapshot.snapshotId}' to database.`);
+        // Add the FileSnapshot as the user's most recent FileSnapshot
+        if (user.filesnapshot) {
+            user.filesnapshot.unshift(newSnapshot.snapshotId);
+        } else {
+            user.filesnapshot = [newSnapshot.snapshotId];
+        }     
+        user.save(); 
+        // Create a File Document for each file in the user's drive
+        await Promise.all(Object.values(Object.fromEntries(fileMap)).map(async function (file) {
+            const newFile = new File({
+                snapshotId: newSnapshot.snapshotId,
+                fileId: file.id,
+                name: file.name,
+                driveId: file.driveId,
+                path: file.path,
+                owner: file.owner,
+                creator: file.creator,
+                sharingUser: file.sharingUser,
+                root: file.root,
+                parent: file.parent,
+                children: file.children,
+                permissionIds: file.permissionIds,
+                permissions: file.permissions,
+                permissionsRaw: file.permissionsRaw,
+                lastModifiedTime: file.modifiedTime,
+            });
+            await File.create(newFile);
+            console.log(`Added File '${newFile.fileId}' to database.`);
+        }));
+        // Perform sharing analysis on the newly created snapshot
+        AnalysisController.sharingAnalysis(newSnapshot.snapshotId, user.threshold);
+        // Send the newly created FileSnapshot's id and owner to the client
+        res.status(200).json({ success: true, fileSnapshot: newSnapshot });
     } catch(error) {
-        console.error(`Failed to create FileSnapshot: ${error}`);
+        console.error(`Failed to create FileSnapshot. ${error}`);
         res.status(400).json({ success: false, error: error });   
     }
-};
-
-// Retrieve a FileSnapshot given its Id
-getSnapshot = async function(req, res) {
-    // Retrieve Id of FileSnapshot from request
-    const snapshotId = req.body.id;
-    try {
-        const snapshot = await FileSnapshot.findOne({ snapshotId: snapshotId });
-        if (!snapshot) {
-            throw new Error('Unable to find FileSnapshot in database.');
-        }
-        res.status(200).json({ success: true, snapshot: snapshot });
-    } catch(error) {
-        console.error(`Failed to retrieve FileSnapshot: ${error}`);
-        res.status(400).json({ success: false, error: error });
-    }
-};
+}
 
 // Retrieves files given a snapshot's Id and list of file Ids
-getFiles = async function(req, res) {
+async function getFiles(req, res) {
     // Retrieve snapshot and file Ids from request
     const { snapshotId } = req.params;
     const fileIds = req.body.fileIds;
@@ -63,10 +100,10 @@ getFiles = async function(req, res) {
         console.error(`Failed to retrieve files: ${error}`);
         res.status(400).json({ success: false, error: error });
     }
-};
+}
 
 // Retrieve the content of a folder given its Id
-getFolder = async function(req, res) {
+async function getFolder(req, res) {
     // Retrieve FileSnapshot and folder Ids from request
     const { snapshotId, folderId } = req.params;
     try {
@@ -90,12 +127,28 @@ getFolder = async function(req, res) {
         console.error('Failed to find folder: ' + error);
         res.status(400).json({ success: false, error: error });   
     }
-};
+}
+
+// Retrieve a FileSnapshot given its Id
+async function getSnapshot(req, res) {
+    // Retrieve Id of FileSnapshot from request
+    const snapshotId = req.body.id;
+    try {
+        const snapshot = await FileSnapshot.findOne({ snapshotId: snapshotId });
+        if (!snapshot) {
+            throw new Error('Unable to find FileSnapshot in database.');
+        }
+        res.status(200).json({ success: true, snapshot: snapshot });
+    } catch(error) {
+        console.error(`Failed to retrieve FileSnapshot: ${error}`);
+        res.status(400).json({ success: false, error: error });
+    }
+}
 
 // Delete all files stored in the database
-deleteFiles = async function(req, res) {
+async function deleteFiles(req, res) {
     // const owner = '634cb4405445ff8fb73a6749';    // Bennie
-    // const owner = '63622af03f1cede505453ce6';    // Richard
+    // const owner = '63845aaad2866c06db380031';    // Richard
     // const owner = '6358b63f68f6810c650732af';    // Brandon
     let snapshotList = await FileSnapshot.find({ owner: owner });
     await Promise.all(snapshotList.map(async (snapshot) => {
@@ -109,7 +162,7 @@ deleteFiles = async function(req, res) {
     // await User.updateMany({}, { $set: { filesnapshot: [] } });
 
     res.send('All files deleted');
-};
+}
 
 module.exports = {
     createFileSnapshot,
